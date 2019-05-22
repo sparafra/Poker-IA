59ae479de135423d557b8e4c43130025b3fbb360
francesco <francesco@francesco-XPS-15-9560>
2019-05-22 23:09:52 +0200

    up

--- Poker/src/main/java/Game/Game.java
+++ Poker/src/main/java/Game/Game.java
@@ -111,7 +111,7 @@ private static final int PADDING = 5;
 		
 		
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
